58005ddaca02688eb7c31bf8b378c15e030b6a0b
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2023-10-24 05:46:19 -0500

    authentication modal

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,6 +5,11 @@ import { twMerge } from 'tailwind-merge';
 import { RxCaretLeft, RxCaretRight } from 'react-icons/rx';
 import { HiHome } from 'react-icons/hi';
 import { BiSearch } from 'react-icons/bi';
+import { FaUserAlt } from 'react-icons/fa';
+import { useSupabaseClient } from '@supabase/auth-helpers-react';
+import useAuthModal from '@/hooks/useAuthModal';
+import { useUser } from '@/hooks/useUser';
+import { toast } from 'react-hot-toast';
 import Button from './Button';
 
 interface HeaderProps {
@@ -13,10 +18,25 @@ interface HeaderProps {
 }
 
 const Header: React.FC<HeaderProps> = ({ children, className }) => {
+	const authModal = useAuthModal();
+
 	const router = useRouter();
 
-	const handleLogout = () => {
-		// Handle logout in the future
+	const supabaseClient = useSupabaseClient();
+
+	const { user } = useUser();
+
+	const handleLogout = async () => {
+		const { error } = await supabaseClient.auth.signOut();
+
+		// TODO: Reset any playing songs
+		router.refresh();
+
+		if (error) {
+			toast.error(error.message);
+		} else {
+			toast.success('Logged out!');
+		}
 	};
 
 	return (
@@ -52,21 +72,38 @@ const Header: React.FC<HeaderProps> = ({ children, className }) => {
 				</div>
 
 				<div className="flex justify-between items-center gap-x-4">
-					<>
-						<div>
+					{user ? (
+						<div className="flex gap-x-4 items-center">
+							<Button onClick={handleLogout} className="bg-white px-6 py-2">
+								Logout
+							</Button>
 							<Button
-								onClick={() => {}}
-								className="bg-transparent text-neutral-300 font-medium"
+								onClick={() => router.push('/account')}
+								className="bg-white"
 							>
-								Sign up
+								<FaUserAlt />
 							</Button>
 						</div>
-						<div>
-							<Button onClick={() => {}} className="bg-white px-6 py-2">
-								Log in
-							</Button>
-						</div>
-					</>
+					) : (
+						<>
+							<div>
+								<Button
+									onClick={authModal.onOpen}
+									className="bg-transparent text-neutral-300 font-medium"
+								>
+									Sign up
+								</Button>
+							</div>
+							<div>
+								<Button
+									onClick={authModal.onOpen}
+									className="bg-white px-6 py-2"
+								>
+									Log in
+								</Button>
+							</div>
+						</>
+					)}
 				</div>
 			</div>
 			{children}
---
+++ providers/ModalProvider.tsx
@@ -0,0 +1,24 @@
+'use client';
+
+import { useState, useEffect } from 'react';
+import AuthModal from '@/components/AuthModal';
+
+const ModalProvider = () => {
+	const [isMounted, setIsMounted] = useState(false);
+
+	useEffect(() => {
+		setIsMounted(true);
+	}, []);
+
+	if (!isMounted) {
+		return null;
+	}
+
+	return (
+		<>
+			<AuthModal />
+		</>
+	);
+};
+
+export default ModalProvider;
