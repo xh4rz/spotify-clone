96b0c75cf2616df62f9b3bf295d92bd0cf5fe4f3
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2023-12-22 16:23:07 -0500

    Player functionality

--- components/Library.tsx
+++ components/Library.tsx
@@ -7,6 +7,7 @@ import { useUser } from '@/hooks/useUser';
 import useUploadModal from '@/hooks/useUploadModal';
 import { Song } from '@/types';
 import MediaItem from './MediaItem';
+import useOnPlay from '@/hooks/useOnPlay';
 interface LibraryProps {
 	songs: Song[];
 }
@@ -17,6 +18,8 @@ const Library: React.FC<LibraryProps> = ({ songs }) => {
 	const uploadModal = useUploadModal();
 
 	const { user } = useUser();
+
+	const onPLay = useOnPlay(songs);
 
 	const onClick = () => {
 		if (!user) {
@@ -46,7 +49,11 @@ const Library: React.FC<LibraryProps> = ({ songs }) => {
 
 			<div className="flex flex-col gap-y-2 mt-4 px-3">
 				{songs.map((item) => (
-					<MediaItem onClick={() => {}} key={item.id} data={item} />
+					<MediaItem
+						onClick={(id: string) => onPLay(id)}
+						key={item.id}
+						data={item}
+					/>
 				))}
 			</div>
 		</div>
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -8,6 +8,8 @@ import { Song } from '@/types';
 import Box from './Box';
 import SidebarItem from './SidebarItem';
 import Library from './Library';
+import usePlayer from '@/hooks/usePLayer';
+import { twMerge } from 'tailwind-merge';
 
 interface SidebarProps {
 	children: React.ReactNode;
@@ -16,6 +18,8 @@ interface SidebarProps {
 
 const Sidebar: React.FC<SidebarProps> = ({ children, songs }) => {
 	const pathname = usePathname();
+
+	const player = usePlayer();
 
 	const routes = useMemo(
 		() => [
@@ -36,7 +40,15 @@ const Sidebar: React.FC<SidebarProps> = ({ children, songs }) => {
 	);
 
 	return (
-		<div className="flex h-full">
+		<div
+			className={twMerge(
+				`
+				flex
+				h-full
+			`,
+				player.activeId && 'h-[calc(100%-80px)]'
+			)}
+		>
 			<div className="hidden md:flex flex-col gap-y-2 black h-full w-[300px] p-2">
 				<Box>
 					<div className="flex flex-col gap-y-4 px-5 py-4">
