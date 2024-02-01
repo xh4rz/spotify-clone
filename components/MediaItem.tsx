7c33d79f8c115757c02377d9cdf5287d23266561
Harold Gonzalez <xh4rz@hotmail.com>
2024-02-01 01:23:39 -0500

    deployment

--- components/MediaItem.tsx
+++ components/MediaItem.tsx
@@ -3,6 +3,7 @@
 import Image from 'next/image';
 import useLoadImage from '@/hooks/useLoadImage';
 import { Song } from '@/types';
+import usePlayer from '@/hooks/usePLayer';
 
 interface MediaItemProps {
 	data: Song;
@@ -10,6 +11,8 @@ interface MediaItemProps {
 }
 
 const MediaItem: React.FC<MediaItemProps> = ({ data, onClick }) => {
+	const player = usePlayer();
+
 	const imageUrl = useLoadImage(data);
 
 	const handleClick = () => {
@@ -17,7 +20,7 @@ const MediaItem: React.FC<MediaItemProps> = ({ data, onClick }) => {
 			return onClick(data.id);
 		}
 
-		// TODO: Default turn on player
+		return player.setId(data.id);
 	};
 
 	return (
